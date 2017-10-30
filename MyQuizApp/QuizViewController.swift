4ffba6b02157a1f6a1053c4533b47db0eb42911b
MacStudent <macstudent@20X-iMacXX-M-6.local>
2017-10-30 14:24:48 -0400

    Chnage in Login UI

--- MyQuizApp/QuizViewController.swift
+++ MyQuizApp/QuizViewController.swift
@@ -24,7 +24,6 @@ class QuizViewController: UIViewController {
     var seconds = 30
     var timer = Timer()
     var isTimerRunning = true
-    
     let questionnaire = QuestionnaireViewModel().loadQuestion()
     var pickedAnswer : String = ""
     var questionNumber : Int = 0
@@ -34,7 +33,7 @@ class QuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         runTimer()
-        lblHighScore.text = "High Score: " + String(MyResultViewModel.highScore())
+        lblHIghScore.text = "High Score: " + String(MyResultViewModel.highScore())
         self.questionNumber = 0
         self.score = 0
         btnOption1.layer.cornerRadius = 10
@@ -67,5 +66,145 @@ class QuizViewController: UIViewController {
         pickedAnswer = (sender.titleLabel?.text)!
         self.initialiseButtonColor()
         sender.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func btnNext_Click(_ sender: UIButton) {
+        goToNextQuestion()
+    }
+    
+    @IBAction func btnTurnOn_Click(_ sender: UIBarButtonItem) {
+        self.isSoundOn = !self.isSoundOn
+        if(isSoundOn){
+            let img = UIImage(named: "ic_volume_up")
+            sender.image = img
+        }
+        else{
+            let img = UIImage(named: "ic_volume_off")
+            sender.image = img
+        }
+    }
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(QuestionnaireViewController.updateTimer)), userInfo: nil, repeats: true)
+        
+    }
+    @objc func updateTimer() {
+        seconds -= 1
+        if(seconds == 0){
+            goToNextQuestion()
+        }
+        else if(seconds < 0){
+            lblTime.text = ""
+        }
+        lblTime.text = "Timer: " + String(seconds) + "sec"
+    }
+    
+    func nextQuestion() {
+        TextToSpeech.synth.stopSpeaking(at: .immediate)
+        if questionNumber < 10 {
+            progressBar.frame.size.width = (view.frame.size.width / 10) * CGFloat(questionNumber)
+            progressLabel.text = String(questionNumber) + "/10"
+            // TextToSpeech.stopSpeech()
+            lblQuestionNo.text = "Question No \(self.questionNumber+1)"
+            lblQuestion.text = questionnaire[questionNumber].questionText
+            btnOption1.setTitle(questionnaire[questionNumber].options[0], for: .normal)
+            btnOption2.setTitle(questionnaire[questionNumber].options[1], for: .normal)
+            btnOption3.setTitle(questionnaire[questionNumber].options[2], for: .normal)
+            btnOption4.setTitle(questionnaire[questionNumber].options[3], for: .normal)
+            if(isSoundOn){
+                TextToSpeech.textToSpeecg(text: questionnaire[questionNumber].questionText)
+                TextToSpeech.arrayToSpeech(options: questionnaire[questionNumber].options)
+            }
+            
+        }
+        else {
+            let alert = UIAlertController(title: "Result", message: "Your score is \(self.score). Do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { UIAlertAction in
+                self.startOver()
+            })
+            
+            alert.addAction(restartAction)
+            MyResultViewModel.addScore(score: self.score)
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    //This method will check if the user has got the right answer.
+    func checkAnswer()  {
+        if(questionNumber < 10){
+            let correctAnswer = questionnaire[questionNumber].correctAnswer
+            if correctAnswer == pickedAnswer {
+                if (btnOption1.titleLabel?.text == correctAnswer){
+                    btnOption1.backgroundColor = UIColor.green
+                }
+                else if (btnOption2.titleLabel?.text == correctAnswer){
+                    btnOption2.backgroundColor = UIColor.green
+                }
+                else if (btnOption3.titleLabel?.text == correctAnswer){
+                    btnOption3.backgroundColor = UIColor.green
+                }
+                else if (btnOption4.titleLabel?.text == correctAnswer){
+                    btnOption4.backgroundColor = UIColor.green
+                }
+                score = score + 1
+            }
+            else {
+                if (btnOption1.titleLabel?.text == pickedAnswer){
+                    btnOption1.backgroundColor = UIColor.red
+                }
+                else if (btnOption2.titleLabel?.text == pickedAnswer){
+                    btnOption2.backgroundColor = UIColor.red
+                }
+                else if (btnOption3.titleLabel?.text == pickedAnswer){
+                    btnOption3.backgroundColor = UIColor.red
+                }
+                else if (btnOption4.titleLabel?.text == pickedAnswer){
+                    btnOption4.backgroundColor = UIColor.red
+                }
+                
+            }
+            if (btnOption1.titleLabel?.text == correctAnswer){
+                btnOption1.backgroundColor = UIColor.green
+            }
+            else if (btnOption2.titleLabel?.text == correctAnswer){
+                btnOption2.backgroundColor = UIColor.green
+            }
+            else if (btnOption3.titleLabel?.text == correctAnswer){
+                btnOption3.backgroundColor = UIColor.green
+            }
+            else if (btnOption4.titleLabel?.text == correctAnswer){
+                btnOption4.backgroundColor = UIColor.green
+            }
+            lblScore.text = "Score: " + String(score)
+        }
+    }
+    
+    
+    func startOver() {
+        self.viewDidLoad()
+        //  questionNumber = 0
+        ///score = 0
+        //nextQuestion()
+        
+    }
+    
+    func initialiseButtonColor()  {
+        btnOption1.backgroundColor = UIColor.white
+        btnOption2.backgroundColor = UIColor.white
+        btnOption3.backgroundColor = UIColor.white
+        btnOption4.backgroundColor = UIColor.white
+    }
+    func goToNextQuestion() {
+        self.checkAnswer()
+        self.seconds = 31
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            self.initialiseButtonColor()
+            self.questionNumber = self.questionNumber + 1
+            self.nextQuestion()
+            if(self.questionNumber == 9){
+                self.btnNext.titleLabel?.text = "Finish"
+            }
+            
+        })
     }
 }
